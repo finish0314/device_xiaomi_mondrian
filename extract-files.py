663904c7bf9115bdd9ce8de40a910c7cb7a88af4
Arian <arian.kulmer@web.de>
2025-01-27 23:21:19 +0100

    mondrian: Import audio configs from LA.VENDOR.1.0.r1-26900-WAIPIO.QSSI15.0
    Change-Id: Ie7e6f1c3a41d6b800ae3501c4afff8e898ff8dad

--- extract-files.py
+++ extract-files.py
@@ -46,8 +46,8 @@ blob_fixups: blob_fixups_user_type = {
         'vendor.qti.hardware.display.config-V2-ndk.so',
     ),
     'vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service': blob_fixup().add_needed(
-    'libstagefright_foundation-v33.so',
-    )
+        'libstagefright_foundation-v33.so'
+    ),
     (
         'vendor/lib64/libTrueSight.so',
         'vendor/lib64/libalAILDC.so',
