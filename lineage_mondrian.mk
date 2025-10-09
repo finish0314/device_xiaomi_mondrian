00ec4d188c9793b58377a98a1ae641e72a88944b
finish0314 <finish21190@gmail.com>
2025-10-10 03:21:32 +0800

    mondrian: Update blobs and firmware from OS2.0.213.0.VMNCNXM

--- lineage_mondrian.mk
+++ lineage_mondrian.mk
@@ -13,13 +13,15 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_mondrian
 PRODUCT_DEVICE := mondrian
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := 23013PC75G
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 23013RK75C
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="mondrian_global-user 15 AQ3A.240912.001 OS2.0.203.0.VMNMIXM release-keys" \
-    BuildFingerprint=POCO/mondrian_global/mondrian:15/AQ3A.240912.001/OS2.0.203.0.VMNMIXM:user/release-keys \
+    BuildDesc="mondrian-user 15 SKQ1.240401.001 OS2.0.213.0.VMNCNXM release-keys" \
+    BuildFingerprint=Redmi/mondrian/mondrian:15/SKQ1.230401.001/OS2.0.213.0.VMNCNXM:user/release-keys \
+    DeviceName=mondrian \
     DeviceProduct=mondrian \
-    SystemName=mondrian_global
+    SystemDevice=mondrian \
+    SystemName=mondrian
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
