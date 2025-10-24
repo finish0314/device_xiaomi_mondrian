e01a0becb809a6321b16a0e1a2f9604728072903
Arian <arian.kulmer@web.de>
2025-10-24 16:55:50 +0200

    mondrian: Update blobs and firmware from OS2.0.207.0.VMNMIXM
    Change-Id: I11b1d232748a60da56b1e78e34aaf2f7c7d3afd0

--- lineage_mondrian.mk
+++ lineage_mondrian.mk
@@ -13,12 +13,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_mondrian
 PRODUCT_DEVICE := mondrian
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := 23013RK75C
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 23013PC75G
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="mondrian-user 15 SKQ1.240401.001 OS2.0.213.0.VMNCNXM release-keys" \
-    BuildFingerprint=Redmi/mondrian/mondrian:15/SKQ1.230401.001/OS2.0.213.0.VMNCNXM:user/release-keys \
+    BuildDesc="mondrian_global-user 15 AQ3A.240912.001 OS2.0.207.0.VMNMIXM release-keys" \
+    BuildFingerprint=POCO/mondrian_global/mondrian:15/AQ3A.240912.001/OS2.0.207.0.VMNMIXM:user/release-keys \
     DeviceName=mondrian \
     DeviceProduct=mondrian \
     SystemDevice=mondrian \
