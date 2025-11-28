4ea85142c8d8c4958727a450d09523c1c23fe1fb
finish0314 <finish21190@gmail.com>
2025-11-28 20:16:46 +0800

    mondrian: Bringup for avium

--- lineage_mondrian.mk
+++ lineage_mondrian.mk
@@ -25,3 +25,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     SystemName=mondrian
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+AVIUM_BUILDTYPE := Release
+AVIUM_IS_OFFICIAL := true
+AVIUM_MAINTAINER := Finish
+
